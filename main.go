2ee6dbe720f6dd27ef4f7029ca094e8d139c7b4a
Simon Yen <simonyen@foxmail.com>
2022-11-10 21:10:33 +0800

    项目结构搭好了

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
-import "github.com/kataras/iris/v12"
+import (
+	"jyoyuu/controllers"
+	"jyoyuu/views"
+
+	"github.com/kataras/iris/v12"
+)
 
 func main() {
 	app := iris.New()
-	engine := iris.HTML("./", ".html")
-	app.RegisterView(engine)
-	app.Get("/", func(ctx iris.Context) {
-		ctx.WriteString("こんにちは！")
-	})
-	app.Get("/hello", func(ctx iris.Context) {
-		ctx.ViewData("title", "你好")
-		ctx.ViewData("content", "女优库")
-		ctx.View("HelloWorld.html")
-	})
+	views.Init(true)
+	app.RegisterView(views.Engine)
+	app.HandleDir("/assets", iris.Dir("./views/static"))
+	app.Get("/", controllers.HomeController)
 	app.Run(iris.Addr(":9999"), iris.WithCharset("UTF-8"))
 }
